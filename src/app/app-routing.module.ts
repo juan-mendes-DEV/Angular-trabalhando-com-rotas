d85e4984029556d1c7672fdda6a85bd5d9c24482
Juan pablo mendes <jp436807@gmail.com>
2024-02-13 09:19:38 -0300

    trabalhando co rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {
+  path:'', component: TitleComponent, pathMatch:'full'
+ },
+ {
+  path:'portfolio', component:CardComponent, children:[//rota principal
+    {path:':id', component:CardComponent},//filho
+    {path:':id/:token', component:CardComponent}//filho
+  ]
+ },
+ {
+  path:'**', redirectTo:''
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {IndexModule} from './pages/index/index.module'; //importando modulos criado
+import {PortifolioModule} from './pages/portifolio/portifolio.module';
+import { MenuNavComponent } from './shared/menu-nav/menu-nav.component'; //importando modulos criado
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IndexModule, //importando modulos criado
+    PortifolioModule //importando modulos criado
   ],
   providers: [],
   bootstrap: [AppComponent]
